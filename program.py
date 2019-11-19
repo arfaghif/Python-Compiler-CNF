b44ed7b0dc28e69a25c51dc90f9539bb10f1c051
Raven <raven_claw27@yahoo.com>
2019-11-19 21:10:16 +0700

    Update program.py

--- program.py
+++ program.py
@@ -3,9 +3,20 @@
 import keyword as key
 
 
+def ignore_tab(X):
+	temp = '\t'
+	duar = ""
+	for i in (X):
+		if((i != '\') and (i+1 != 't')):
+			duar = duar + i
+	return duar
+
 with open('test.txt', 'r') as f:
 	print(f.name)
 	f_contents = f.readlines()
+	jmlbaris = len(f_contents)
+	for i in range (jmlbaris):
+		print(f_contents[i])
+		
 	print(f_contents)
-
-	print(key.kwlist)
+	#print(key.kwlist)
