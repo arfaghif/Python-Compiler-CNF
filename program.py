7b7be741f51857edf181d02c1075e127f30e9812
arfaghif <52845090+arfaghif@users.noreply.github.com>
2019-11-19 21:04:03 +0700

    nambah ignore

--- program.py
+++ program.py
@@ -6,6 +6,11 @@ import keyword as key
 with open('test.txt', 'r') as f:
 	print(f.name)
 	f_contents = f.readlines()
+	#f_contents = [line[:-1] for line in f_contents]
+	alist = []
+	for i in f_contents:
+		alist.append(i.replace('\t',"").replace('\n',''))
 	print(f_contents)
+	print(alist)
 
 	print(key.kwlist)
