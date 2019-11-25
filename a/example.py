89769cab2d34e8eb0707a974f49d5d37b854abeb
arfaghif <52845090+arfaghif@users.noreply.github.com>
2019-11-25 23:56:39 +0700

    kita kerja d folder a

---
+++ a/example.py
@@ -0,0 +1,26 @@
+#usage: pyhton3 example.py
+
+from CYK_Paser import Grammar
+
+g = Grammar('example_grammar1.txt')
+g.parse('c')
+g.print_parse_table()
+
+print('')
+print('')
+print('')
+"""
+g = Grammar('example_grammar2.txt')
+g.parse(' 2')
+#g.print_parse_table()
+
+print('')
+print('')
+print('')
+
+g = Grammar('example_grammar2.txt')
+g.parse('eats she fork a fish')
+g.print_parse_table()
+#g.get_trees()
+"""
+
--- program.py
+++ program.py
@@ -3,21 +3,8 @@
 import keyword as key
 
 
-<<<<<<< HEAD
 with open('grammar.txt', 'r') as f:
 	"""print(f.name)
-=======
-def ignore_tab(X):
-	temp = '\t'
-	duar = ""
-	for i in (len(X)):
-		if(((i != (len(X)-1)) and (X[i] != '\'') and (X[i+1] != 't')) or (X[i] == 't')):
-			duar = duar + X[i]
-	return duar
-
-with open('test.txt', 'r') as f:
-	print(f.name)
->>>>>>> 2ef87e6e4ef68951cf504a1f436850880c1f4e0c
 	f_contents = f.readlines()
 	jmlbaris = len(f_contents)
 	for i in range (jmlbaris):
@@ -40,10 +27,6 @@ with open('test.txt', 'r') as f:
 		li.append(i.replace('\t',"").replace('\n','').split(" "))
 
 	print(key.kwlist)
-<<<<<<< HEAD
 	print(li)"""
 	f1 = f.read().split("->")
 	print(f1)
-=======
-	print(li)
->>>>>>> 2ef87e6e4ef68951cf504a1f436850880c1f4e0c
